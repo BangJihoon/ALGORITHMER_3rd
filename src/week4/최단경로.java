e15eb1577bfb0d33bc9c0f4d9268304c0dcab613
bang jihoon <jihoon289@naver.com>
2022-01-18 18:37:49 +0900

    daliy coding

--- src/week4/최단경로.java
+++ src/week4/최단경로.java
@@ -1,5 +1,24 @@
 package week4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
 public class 최단경로 {
-	// 다시풀기
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			int input[] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();		
+			int start = Integer.parseInt(br.readLine());
+			int[] ans = new int[input[0]];
+			int lines[][] = new int[input[1]][3];
+			for(int i=0;i<input[1];i++) {
+				lines[i] = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+				System.out.println(lines[i][0]+" "+lines[i][1]);
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }

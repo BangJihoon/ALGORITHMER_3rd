5031b41572755947302bcabd4040c5fc852641e5
bang jihoon <jihoon289@naver.com>
2022-02-15 18:25:49 +0900

    설정파일 추가

---
+++ src/week12/test.java
@@ -0,0 +1,10 @@
+package week12;
+
+public class test {
+    public static void main(String[] args) {
+        int[] numbers = {3, 30, 34, 5, 9};
+        for(int i : numbers)
+            System.out.println(Integer.toString(i).charAt(0));
+        System.out.println("987".charAt(0) - '5');
+    }
+}

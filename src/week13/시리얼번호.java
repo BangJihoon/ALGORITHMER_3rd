4933f51b4f49fbe9f36dbdd49a6370d136d0f3e1
bang jihoon <jihoon289@naver.com>
2022-02-17 13:09:21 +0900

    week13

--- src/week13/시리얼번호.java
+++ src/week13/시리얼번호.java
@@ -1,7 +1,37 @@
 package week13;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+// 15,460 kb , 140 ms
 public class 시리얼번호 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for(int i=0;i<n;i++) arr[i] = br.readLine();
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1.length() - o2.length()==0){
+                int a = getSum(o1);
+                int b = getSum(o2);
+                if(a - b == 0){
+                    return o1.compareTo(o2);
+                }
+                else {
+                    return a - b;
+                }
+            }else {
+                return o1.length() - o2.length();
+            }
+        });
+        for (String s : arr) System.out.println(s);
+    }
+    public static int getSum(String s){
+        int a=0;
+        s = s.replaceAll("[^0-9]","");
+        for(char c : s.toCharArray()) a+=Character.getNumericValue(c);
+        return a;
     }
 }

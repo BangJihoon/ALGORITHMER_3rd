62d6977a2c9838cb0b19e950d4fba4e874e93790
BangJihoon <jihoon289@naver.com>
2022-03-06 22:34:34 +0900

    Update 다리를지나는트럭.java

--- src/week15/다리를지나는트럭.java
+++ src/week15/다리를지나는트럭.java
@@ -2,7 +2,23 @@ package week15;
 
 import java.util.LinkedList;
 import java.util.Queue;
-
+/*
+정확성  테스트
+테스트 1 〉	통과 (1.19ms, 78.9MB)
+테스트 2 〉	통과 (8.66ms, 80.9MB)
+테스트 3 〉	통과 (0.14ms, 80.1MB)
+테스트 4 〉	통과 (9.20ms, 82.2MB)
+테스트 5 〉	통과 (25.49ms, 92.4MB)
+테스트 6 〉	통과 (12.92ms, 85.7MB)
+테스트 7 〉	통과 (1.06ms, 65.8MB)
+테스트 8 〉	통과 (0.59ms, 77.2MB)
+테스트 9 〉	통과 (2.63ms, 67.2MB)
+테스트 10 〉	통과 (0.62ms, 74.4MB)
+테스트 11 〉	통과 (0.18ms, 74.3MB)
+테스트 12 〉	통과 (0.78ms, 74.5MB)
+테스트 13 〉	통과 (2.06ms, 74MB)
+테스트 14 〉	통과 (0.20ms, 80.2MB)
+*/
 public class 다리를지나는트럭 {
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<Integer> q = new LinkedList<Integer>();

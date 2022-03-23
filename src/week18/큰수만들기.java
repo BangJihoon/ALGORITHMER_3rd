6d7a991b2457b6824dc64d1a02614acf1fae4c6e
BangJihoon <jihoon289@naver.com>
2022-03-23 21:12:15 +0900

    Update 큰수만들기.java

--- src/week18/큰수만들기.java
+++ src/week18/큰수만들기.java
@@ -2,7 +2,21 @@ package week18;
 
 import java.util.Iterator;
 import java.util.Stack;
-
+/*
+정확성  테스트
+테스트 1 〉	통과 (0.31ms, 75.2MB)
+테스트 2 〉	통과 (0.29ms, 74.4MB)
+테스트 3 〉	통과 (0.50ms, 76.7MB)
+테스트 4 〉	통과 (1.43ms, 73.1MB)
+테스트 5 〉	통과 (2.53ms, 80.1MB)
+테스트 6 〉	통과 (8.14ms, 71.7MB)
+테스트 7 〉	통과 (14.43ms, 74.5MB)
+테스트 8 〉	통과 (17.99ms, 83.6MB)
+테스트 9 〉	통과 (45.06ms, 84.2MB)
+테스트 10 〉	통과 (87.72ms, 101MB)
+테스트 11 〉	통과 (0.27ms, 67.5MB)
+테스트 12 〉	통과 (0.26ms, 69.7MB)
+*/
 public class 큰수만들기 {
     public static void main(String[] args) {
         //ans = 999999955285407310941966307933122347117

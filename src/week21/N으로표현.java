ab85ecfa17bfa55f88c0b2e180deadf56b8865ed
BangJihoon <jihoon289@naver.com>
2022-04-10 23:59:19 +0900

    week21 DP init

---
+++ src/week21/N으로표현.java
@@ -0,0 +1,16 @@
+package week21;
+
+public class N으로표현 {
+    public static void main(String[] args) {
+        // 5.12.4
+        int N = 5;
+        int number = 12;
+        System.out.println(solution(N,number));
+    }
+    public static void dfs(int N, int number, int count, int sum) {
+
+    }
+    public static  int solution(int N, int number) {
+        return 1;
+    }
+}
---
+++ src/week21/정수삼각형.java
@@ -0,0 +1,11 @@
+package week21;
+
+public class 정수삼각형 {
+    public static void main(String[] args) {
+        int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}; // 30
+        System.out.println(solution(triangle));
+    }
+    public static int solution(int[][] triangle) {
+        return 1;
+    }
+}

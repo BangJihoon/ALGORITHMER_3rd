67ef2afe90d410ed012ae7c0ec4144bac902509d
BangJihoon <jihoon289@naver.com>
2022-05-06 17:50:39 +0900

    week24 - Solved 팰린드롬만들기 , 뭐때문에 안될까,,?

--- src/week24/팰린드롬만들기.java
+++ src/week24/팰린드롬만들기.java
@@ -1,7 +1,37 @@
 package week24;
 
+import java.util.Scanner;
+
 public class 팰린드롬만들기 {
     public static void main(String[] args) {
-
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        sc.close();
+        // 계산
+        int res = count_diff(0,n-1,arr);
+        // 결과
+        System.out.println(res);
+    }
+    static int count_diff(int left , int right, int[] arr){
+        int res=0;
+        for (int i=left ; i<right; i++){
+            if(arr[i] != arr[right])
+                res++;
+            else
+                right--;
+        }
+        int min = res;
+        res =0;
+        for (int i=right ; i>left; i--){
+            if(arr[i] != arr[left])
+                res++;
+            else
+                left++;
+        }
+        return Math.min(min,res);
     }
 }
